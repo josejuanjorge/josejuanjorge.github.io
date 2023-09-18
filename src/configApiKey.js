50201b6761230b40c149bd94c0ecc7b5a23c4eb1
Jose J. Jorge C <58875186+josejuanjorge@users.noreply.github.com>
2023-09-18 23:13:49 +0200

    Update configApiKey.js

--- src/configApiKey.js
+++ src/configApiKey.js
@@ -1,5 +1,1 @@
-// accessToken.js
-const accessToken = 'pk.eyJ1IjoiampqYyIsImEiOiJjbG1rZWg0dzAwMXExMnJyeDFsb3g3MnhlIn0.NHcGliHgSMsGPNOQFwnBTQ';
 
-// Export the access token
-module.exports = accessToken;
