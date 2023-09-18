0198e4669527e61e73d098b8881e97dd488141e3
Jose J. Jorge C <58875186+josejuanjorge@users.noreply.github.com>
2023-09-18 22:39:22 +0200

    Create configApiKey.js

---
+++ src/configApiKey.js
@@ -0,0 +1,5 @@
+// accessToken.js
+const accessToken = 'pk.eyJ1IjoiampqYyIsImEiOiJjbG1rZWg0dzAwMXExMnJyeDFsb3g3MnhlIn0.NHcGliHgSMsGPNOQFwnBTQ';
+
+// Export the access token
+module.exports = accessToken;
